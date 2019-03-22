c4f6a7cb7ee3e8a46d794bcc06af5d43eed2b97a
Muhammad Rifki Adam Albarisyi <rifki7080@gmail.com>
2019-03-22 20:12:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "dicodingazurewebserver.database.windows.net";
+    $host = "dicodingazurewebserver";
     $user = "dicodingazure";
     $pass = "Mzbm21417";
     $db = "db_latihan_azure";
@@ -43,7 +43,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO dbo.Registration (name, email, job, date) 
+            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
